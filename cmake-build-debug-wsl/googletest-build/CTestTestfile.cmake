f7d97e133139edd4909050b1608984a36d43d251
213ae <72477864+213ae@users.noreply.github.com>
2023-02-02 21:36:25 +0800

    Delete cmake-build-debug-wsl directory

--- cmake-build-debug-wsl/googletest-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Repos/minisql-master/thirdparty/googletest
-# Build directory: /mnt/d/Repos/minisql-master/cmake-build-debug-wsl/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
